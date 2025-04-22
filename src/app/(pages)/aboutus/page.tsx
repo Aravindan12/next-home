b12867188c6d4f8a390b4e474a1f780f226fb354
DIGITAL <a.aravindan@novactech.in>
2025-04-22 10:49:48 +0530

    first commit

---
+++ src/app/(pages)/aboutus/page.tsx
@@ -0,0 +1,10 @@
+// app/aboutus/page.tsx
+export default function AboutUsPage() {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>This is the About Us page of your app.</p>
+      </div>
+    );
+  }
+  
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,27 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+         <nav style={{ padding: '1rem', background: '#f4f4f4' }}>
+          <Link href="/" style={{ marginRight: '1rem' }}>Home</Link>
+          <Link href="/aboutus">About Us</Link>
+        </nav>
         {children}
+        <Script
+            id="olark-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+                                __html: `(function(o,l,a,r,k,y){if(o.olark)return;
+              r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0];
+              y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r);
+              y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)};
+              y.extend=function(i,j){y("extend",i,j)};
+              y.identify=function(i){y("identify",k.i=i)};
+              y.configure=function(i,j){y("configure",i,j);k.c[i]=j};
+              k=y._={s:[],t:[+new Date],c:{},l:a};
+            })(window,document,"static.olark.com/jsclient/loader.js"); 
+            olark.identify('1304-635-10-4939');`,
+                            }}
+          />
       </body>
     </html>
   );
